3883981d4783a6bc2d591161569ebd119f42785e
Stephen O'Leary <stephen.oleary@sophos.com>
2020-12-17 11:52:34 +0000

    added create database statement

---
+++ create_database.sql
@@ -0,0 +1,3 @@
+/* Use this script to create a database, by default it will be created in the default AWS Glue data catalog, if you want something different you probably already know how to do this! This database name will be referenced in all the SQL statements in this repo, so make sure to change them if you do something different here */
+
+CREATE DATABASE interceptx_edr;
